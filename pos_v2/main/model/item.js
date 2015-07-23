3285343c645eb369ded8bd810b7f61ea44624029
liyuanyuan57 <liyuanyuan0507@outlook.com>
2015-07-23 16:22:25 +0800

    在item中添加了两个行为，findItem()和getAllItems()

--- pos_v2/main/model/item.js
+++ pos_v2/main/model/item.js
@@ -4,3 +4,16 @@ function Item(barcode, name, unit, price) {
   this.unit = unit;
   this.price = price || 0.00;
 }
+
+Item.prototype.findItem = function(barcode){
+  var allItems = this.getAllItems();
+  for(var i = 0; i < allItems.length; i++){
+    if(allItems[i].barcode === barcode){
+      return allItems[i];
+     }
+  }
+};
+
+Item.prototype.getAllItems = function(){
+  return loadAllItems();
+};
