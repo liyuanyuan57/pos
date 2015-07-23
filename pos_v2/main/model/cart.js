bca3fbe287095baa92ccd94fac49644a313ba708
liyuanyuan57 <liyuanyuan0507@outlook.com>
2015-07-23 22:50:51 +0800

    获得了cartItems

--- pos_v2/main/model/cart.js
+++ pos_v2/main/model/cart.js
@@ -1,37 +1,19 @@
-function Cart(cartItems) {
-  this.cartItems = cartItems;
+function Cart(){
+  this.cartItems = [];
 }
-
-Cart.prototype.getCartItems = function(){
-  this.cartItems.forEach(function(cartItem){
-    var promotionItem = findPromotionItem(loadPromotions(),cartItem.item.barcode);
-    if(promotionItem){
-      cartItem.discount = getDiscount(promotionItem,cartItem.count);
-      cartItem.promotionFlag = true;
-    }else{
-      cartItem.discount = 0;
-    }
-      cartItem.finalcount = cartItem.count - cartItem.discount;
-    });
-  return this.cartItems;
+Cart.prototype.addCartItem = function(cartItem){
+   var existed = findCartItem(this.cartItems, cartItem.item.barcode);
+   if(existed){
+     existed.count += cartItem.count;
+   } else{
+     this.cartItems.push(new CartItem(cartItem.item, cartItem.count));
+   }
 };
 
-function findPromotionItem(promotionItems,barcode){
-  for(var m = 0;m < promotionItems.length;m++){
-    if(promotionItems[m].barcodes.indexOf(barcode) !== -1){
-    return promotionItems[m];
+function findCartItem(cartItems,barcode){
+  for(var n = 0; n < cartItems.length; n++){
+    if(cartItems[n].item.barcode === barcode){
+      return cartItems[n];
     }
   }
-}
-
-function getDiscount(promotionItem,count){
-  var discount = 0;
-  if(promotionItem.type === 'BUY_TWO_GET_ONE_FREE'){
-    return discount = buyTwoGetOneFree(count);
-    }
-}
-
-function buyTwoGetOneFree(count){
-  var discount = Math.floor(count / 3);
-  return discount;
 }
--- pos_v2/main/model/scanner.js
+++ pos_v2/main/model/scanner.js
@@ -1,12 +1,11 @@
-function Scanner(tag){
-  this.tag = tag;
+function Scanner(){
 }
-Scanner.prototype.scan = function(){
-  var tag = this.tag.split('-');
+Scanner.prototype.scan = function(tag){
+  var tag = tag.split('-');
   var barcode = tag[0];
   var count = parseFloat(tag[1]) || 1;
   var item1 = new Item();
   var item = item1.findItem(barcode);
-  var cartItem = {item: item, count:count};
+  var cartItem = new CartItem(item,count);
   return cartItem;
-}
+};
